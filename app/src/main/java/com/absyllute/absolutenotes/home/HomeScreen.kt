2130a599d48ae4156f3918644e3ed0494b545810
Absyllute <absyllute@gmail.com>
2026-07-16 13:13:22 +0200

    Added FAB for new notes

--- app/src/main/java/com/absyllute/absolutenotes/home/HomeScreen.kt
+++ app/src/main/java/com/absyllute/absolutenotes/home/HomeScreen.kt
@@ -6,14 +6,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.Wallpapers
+import com.absyllute.absolutenotes.R
 import com.absyllute.absolutenotes.ui.theme.AbsoluteNotesTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,7 +29,21 @@ fun HomeScreen() {
                     Text("aNotes")
                 }
             )
+        },
+
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {},
+                text = { Text("New Note") },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.pen_icon ),
+                        contentDescription = "Add a new note"
+                    )
+                }
+            )
         }
+
     ) { paddingValues ->
         Column(
             modifier = Modifier.fillMaxSize()
